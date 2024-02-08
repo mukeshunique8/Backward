26f7d0da4a500b674c5bb7c85206dc2170c31180
mukeshunique8 <121846128+mukeshunique8@users.noreply.github.com>
2024-02-08 18:40:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+let openAll = document.getElementById("btn1");
+let Portals = document.getElementById("btn2");
+let Sheets = document.getElementById("btn3");
+let AI = document.getElementById("btn4");
+
+
+let links = [
+{url:"https://teams.microsoft.com/", target:"_blank", category: "Portals"},
+{url:"https://mail.google.com/mail/u/0/#inbox", target:"_blank", category: "Portals"},
+{url:"https://10fastfingers.com/typing-test/english", target:"_blank", category: "Portals"},
+{url:"https://expert.chegg.com/qna/authoring", target:"_blank", category: "Portals"},
+{url:"https://app.hcmengine.com/FocusEmployeeConnect/Login.aspx?ReturnUrl=%2fFocusEmployeeConnect%2fHeaderMenuNew.aspx", target:"_blank", category: "Portals"},
+{url:"https://portal.fevtutor.com/", target:"_blank", category: "Portals"},
+
+{url:"https://teams.microsoft.com/", target:"_blank", category: "Documents"},
+{url:"https://outlook.office.com/mail/", target:"_blank", category: "Documents"},
+{url:"https://docs.google.com/spreadsheets/d/1SCeALZFMVuaGrzDJrdb2IKttZLjyNWweqM1JB_zDsgI/edit#gid=726450013", target:"_blank", category: "Documents"},
+{url:"https://docs.google.com/spreadsheets/d/1NoJcYTkdqjCFdfhgr0GVJ674ywEmBuymMYQBtW_t0do/edit#gid=919540150", target:"_blank", category: "Documents"},
+{url:"https://nocfocusedumatics-my.sharepoint.com/:x:/g/personal/sedhu_v_focusedumatics_com/EThePEZInQ5Lq-W6jg2_xHsBoysjFf7ZnIWKQg-EYNNKTg?rtime=GSbDSR4E3Eg", target:"_blank", category: "Documents"},
+{url:"https://docs.google.com/spreadsheets/d/1g5tZ1hAEVqsItXVPjrG0RBFBpajfvY5I_3Gb7gg_aJg/edit?pli=1#gid=0", target:"_blank", category: "Documents"},
+
+{url:"https://chat.openai.com/", target:"_blank", category: "AI"},
+{url:"https://chat.openai.com/", target:"_blank", category: "AI"},
+{url:"https://poe.com/", target:"_blank", category: "AI"},
+{url:"https://www.perplexity.ai/", target:"_blank", category: "AI"},
+{url:"https://bard.google.com/", target:"_blank", category: "AI"},
+{url:"https://you.com/", target:"_blank", category: "AI"}
+];
+
+
+
+
+openAll.addEventListener("click",function(){
+links.forEach(function(link){
+  window.open(link.url,"_blank")
+})
+});
+
+Portals.addEventListener("click",function(){
+links.forEach(function(link){
+  if(link.category === "Portals"){
+  window.open(link.url,"_blank")}
+})
+});
+
+Sheets.addEventListener("click",function(){
+links.forEach(function(link){
+  if(link.category === "Documents"){
+  window.open(link.url,"_blank")}
+})
+});
+
+AI.addEventListener("click",function(){
+links.forEach(function(link){
+  if(link.category === "AI"){
+  window.open(link.url,"_blank")}
+})
+});
+
+
+
+
