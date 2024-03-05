6e88ed2efbe44a7337688d567b1f3aeddf571d06
Muthu Kumaran M <121846128+mukeshunique8@users.noreply.github.com>
2024-03-06 03:37:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,19 @@ let links = [
 {url:"https://expert.chegg.com/qna/authoring", target:"_blank", category: "Portals"},
 {url:"https://app.hcmengine.com/FocusEmployeeConnect/Login.aspx?ReturnUrl=%2fFocusEmployeeConnect%2fHeaderMenuNew.aspx", target:"_blank", category: "Portals"},
 {url:"https://portal.fevtutor.com/", target:"_blank", category: "Portals"},
+// {url:"https://teams.microsoft.com/", target:"_blank", category: "Documents"},
 {url:"https://mail.google.com/mail/u/0/#inbox", target:"_blank", category: "Documents"},
-{url:"https://teams.microsoft.com/", target:"_blank", category: "Documents"},
 {url:"https://outlook.office.com/mail/", target:"_blank", category: "Documents"},
 {url:"https://docs.google.com/spreadsheets/d/1SCeALZFMVuaGrzDJrdb2IKttZLjyNWweqM1JB_zDsgI/edit#gid=726450013", target:"_blank", category: "Documents"},
 {url:"https://docs.google.com/spreadsheets/d/1NoJcYTkdqjCFdfhgr0GVJ674ywEmBuymMYQBtW_t0do/edit#gid=919540150", target:"_blank", category: "Documents"},
 {url:"https://nocfocusedumatics-my.sharepoint.com/:x:/g/personal/sedhu_v_focusedumatics_com/EThePEZInQ5Lq-W6jg2_xHsBoysjFf7ZnIWKQg-EYNNKTg?rtime=GSbDSR4E3Eg", target:"_blank", category: "Documents"},
 {url:"https://docs.google.com/spreadsheets/d/1g5tZ1hAEVqsItXVPjrG0RBFBpajfvY5I_3Gb7gg_aJg/edit?pli=1#gid=0", target:"_blank", category: "Documents"},
-{url:"https://chat.openai.com/", target:"_blank", category: "Documents"},
-{url:"https://chat.openai.com/", target:"_blank", category: "Documents"},
+{url:"https://mukeshunique8.github.io/Scripts", target:"_blank", category: "Documents"},
+{url:"https://mukeshunique8.github.io/Scripts/Progress%20report/pg.html", target:"_blank", category: "Documents"},
 
-{url:"https://chat.openai.com/", target:"_blank", category: "AI"},
+// {url:"https://chat.openai.com/", target:"_blank", category: "Documents"},
+
+// {url:"https://chat.openai.com/", target:"_blank", category: "AI"},
 {url:"https://chat.openai.com/", target:"_blank", category: "AI"},
 {url:"https://poe.com/", target:"_blank", category: "AI"},
 // {url:"https://www.perplexity.ai/", target:"_blank", category: "AI"},
@@ -58,6 +60,8 @@ links.forEach(function(link){
   window.open(link.url,"_blank")}
 })
 });
+
+
 
 
 function updateTime() {
